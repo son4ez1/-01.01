818b02bc68bb7d22fb01d857e784632971e8a702
son4ez1 <113089517+son4ez1@users.noreply.github.com>
2023-12-23 12:08:22 +0300

    Add files via upload

---
+++ MalinovskayaEX/MalinovskayaEX/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MalinovskayaEX
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _mainFrame.Navigate(new pages.Page2());
+        }
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            _mainFrame.Navigate(new pages.Page3());
+        }
+
+        private void Button_nazad(object sender, RoutedEventArgs e)
+        {
+            _mainFrame.Navigate(new pages.Page1());
+        }
+    }
+}
